2bcb1d41f166f01fa5c017e7947fb539c42d626c
Vaswaty <vaswatyhalder12@gmail.com>
2023-09-13 12:16:02 +0530

    First Version

---
+++ bootGitApp/src/main/java/org/anudip/bootGitApp/MyController.java
@@ -0,0 +1,20 @@
+package org.anudip.bootGitApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("/hello")
+	public String showHello() {
+		return "Hello";
+		
+	}
+	@GetMapping("/hi")
+	public String showHi() {
+		return "Hi";
+	}
+		
+
+}
